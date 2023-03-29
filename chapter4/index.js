b5be6eb77fbdf86873fbf22115b017bf7b5b9aaa
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-03-29 04:05:16 +0200

    Update 'and' spacing

--- chapter4/index.js
+++ chapter4/index.js
@@ -13,7 +13,7 @@
 // // let age = Number(prompt('What is your age? '));
 // let message;
 // if(age >= 21){
-//     message = "Entry granted and may purchase alcohol"
+//     message = "Entry granted and  may purchase alcohol"
 // }
 // else if(age >= 19){
 //     message = "Entry granted. Not allowed to purchase alcohol"
@@ -72,67 +72,67 @@ let result = 'My Selection: ';
 switch(prize) {
     case 0:
         result += 'Seiko 5 Field Sports SRPG27 Watch';
-        result += ' and';
+        result += ' and ';
         result += 'R800 cash!';
         break;
 
     case 1:
         result += 'UAG Pressure cooker';
-        result += ' and';
+        result += ' and ';
         result += 'R1000 cash!';
         break;
 
     case 2:
         result += ' 55" Samsung Smart TV';
-        result += ' and';
+        result += ' and ';
         result += 'R1000 cash!';
     break;
 
     case 3:
         result += 'Apple iPhone 12 Black 64GB';
-        result += ' and';
+        result += ' and ';
         result += 'R500 cash!';
     break;
 
     case 4:
         result += 'Bremont Jaguar C-Type Chronograph Watch';
-        result += ' and';
+        result += ' and ';
         result += 'R1000 cash!';
     break;
 
     case 5:
         result += 'Beats Fit Pro Earbuds';
-        result += ' and';
+        result += ' and ';
         result += 'R500 cash!';
     break;
 
     case 6:
         result += 'Food Basket';
-        result += ' and';
+        result += ' and ';
         result += 'R100 cash!';
     break;
 
     case 7:
         result += 'Ster-Kinekor Voucher worth R250';
-        result += ' and';
+        result += ' and ';
         result += 'R800 cash!';
     break;
 
     case 8:
         result += 'Rolex Day-Date Watch';
-        result += ' and';
+        result += ' and ';
         result += 'R1000 cash!';
     break;
 
     case 9:
         result += '10-Piece Kitchenware set';
-        result += ' and';
+        result += ' and ';
         result += 'R500 cash!';
     break;
 
     case 10:
         result += '35" Samsung Smart TV';
-        result += ' and';
+        result += ' and ';
         result += 'R900 cash!';
     break;
     
