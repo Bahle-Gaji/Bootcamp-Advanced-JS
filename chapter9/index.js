6a59d1633692f926227051361aa7c82522575d56
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-04-03 04:31:27 +0200

    Complete PE's and Chapter Project

---
+++ chapter9/index.js
@@ -0,0 +1,44 @@
+/*-----Practice exercise 9.1-----*/
+
+// //1.
+// console.dir(window);
+
+// //2. Yes
+
+// //3.
+// console.log(outerHeight, outerWidth, innerHeight, innerWidth);
+
+/*-------------------------------*/
+
+
+
+//-----Window history object
+// console.dir(history);
+
+// window.history.go(-1);
+
+//-----Window navigator object
+// console.dir(window.navigator);
+
+
+
+/*-----Practice exercise 9.2-----*/
+
+// console.log(window.location.protocol);
+// console.log(window.location.href);
+
+/*-------------------------------*/
+
+
+
+//-----The DOM-----
+
+//-----Selecting page elements
+
+// const ele1 = document.querySelector('h1');
+// console.dir(ele1);
+
+// const eles = document.querySelectorAll('.output');
+// console.log(eles);
+
+
