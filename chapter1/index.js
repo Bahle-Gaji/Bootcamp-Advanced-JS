61d9cca78b4b16735d08b23317fc968d55efb713
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-04-05 23:54:03 +0200

    Complete PE's and Chapter Project

---
+++ chapter1/index.js
@@ -0,0 +1,13 @@
+// Practice exercise 1.3
+console.log('some js code');
+
+// Practice exercise 1.4
+let a = 10; // I have set a equal to 10
+
+console.log(a) // This will display the value of 'a' to the console
+
+// Chapter Project
+console.log('Bahle');
+
+/* This is a
+multi-line comment */
